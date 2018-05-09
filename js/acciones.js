d7152e0785352da1ea6fc0e3726ea8f140842dd9
solano123 <38224628+solano123@users.noreply.github.com>
2018-05-09 07:13:21 -0500

    s

--- js/acciones.js
+++ js/acciones.js
@@ -2,10 +2,10 @@
 $(document).ready(function(e){
 	document.addEventListener("diviceready",function(){
 		$('#sonar').tap(function(){
-			navigator.notification.beep(2)
+			navigator.notification.sonar(2)
 		}); //tap sonar
 		$('vibrar').tap(function(){
-			navigator.notification.vibrate(2000);
+			navigator.notification.vibrar(2000);
 		}); //tap vibrar
     },false); //cierre del diveready
 }); //cierre del ready
